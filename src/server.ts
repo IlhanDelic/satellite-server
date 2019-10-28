f9a08d2f7fc898ef6d0df189ac8158c50ab91985
0914619 <0914619@hr.nl>
2019-10-28 16:25:05 +0100

    changed webadres

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import app from './app';
 
 const server = app.listen(app.get("port"), () => {
-    console.log("app is running on http://crownstonehub-test.herokuapp.com/:%d in %s mode",
-        app.get("port"),
+   // console.log("app is running on http://crownstonehub-test.herokuapp.com:%d in %s mode",
+    (app.get("port"),
         app.get("env")
     )
 
