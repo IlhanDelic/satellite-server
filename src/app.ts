c3226df70aa54e0fc6cfeff3985f193a16eea04e
0914619 <0914619@hr.nl>
2019-10-30 11:57:04 +0100

    hello

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, {Application, Response, Request, NextFunction} from 'express';
-const app: Application = express();
 import * as controller from './control'
+const app: Application = express();
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.send("hello");
 });
+
 app.get('/', controller.hi);
 app.post('/hello', controller.hello);
+
 app.listen(process.env.PORT, () => console.log("running"));
--- src/control.ts
+++ src/control.ts
@@ -1,5 +1,7 @@
 import {Response, Request} from "express";
 
+const add = (a:number, b:number): number => a + b;
+
 export let hi =(req: Request, res: Response) => {
     res.send("hello, there");
 };
@@ -8,3 +10,7 @@ export let hello =(req: Request, res: Response) => {
     res.send("how are you?");
 };
 
+export let sum =(req: Request, res: Response) => {
+    console.log(add(9,9));
+    res.send("check your terminal");
+};
